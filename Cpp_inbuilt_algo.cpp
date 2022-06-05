bec64753882f444deb7a3d30f703b81ab6450c70
Tanmay Garg <95227013+Tan-Code-may@users.noreply.github.com>
2022-06-05 12:58:01 +0530

    C++ STL inbuilt algo uploaded

---
+++ Cpp_inbuilt_algo.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	vector<int> v(n);
+	for(int i=0;i<n;i++)
+		cin>>v[i];
+
+	// All these functions are working in the same way as they
+	// used to when we implement them normally, i.e., most of
+	// them have O(n) as TC, except that 'count' and 'find'
+	// function will have log(n) as TC only in case of maps and
+	// sets.
+
+	int min = *min_element(v.begin(), v.end());
+	cout<<min<<"\n";
+	
+	int max = *max_element(v.begin(), v.end());
+	cout<<max<<"\n";
+	
+	// Both max and min functions returns a pointer, hence we
+	// need to dereference them in order to print them.
+
+	int sum = accumulate(v.begin(), v.end(), 0);
+	cout<<sum<<"\n";
+	// Here last argument specifies the initial sum
+
+	int ct = count(v.begin(), v.end(), 2);
+	cout<<ct<<"\n";
+	// Here last argument specifies the value whose count is needed.
+
+	auto it = find(v.begin(), v.end(), 9);
+	if(it != v.end())
+		cout<<*it<<"\n";
+	else
+		cout<<"Element not found\n";
+	// This function returns an iterator in case of vectors
+	// and a pointer in case of arrays of the element that
+	// need to be found.
+
+	reverse(v.begin(), v.end());
+	for(auto val:v)
+		cout<<val<<" ";
+	cout<<"\n";
+
+	string s="aauhiaerngaej";
+	reverse(s.begin() + 1, s.end());
+	cout<<s<<"\n";
+
+	// reverse() function is quite useful as it can reverse
+	// an array, a vector or a string in a single line of code.
+
+	//Lambda Functions-  short way to define any function.
+	auto add = [](int x, int y){return x+y;};
+	cout<< add(12,23) <<"\n";
+	// Lambda functions can simple be assigned to a variable
+	// by using 'auto' and can be called as normal functions.
+
+	vector<int> a = {1,3,-2,-5,42};
+
+	// All these 3 functions returns a bool value.
+	cout<< all_of(a.begin(), a.end(),[](int x){return x>0;})<<"\n";
+	// returns 1 if all the elements of the vector satisfies
+	// the given function.
+
+	cout<< any_of(a.begin(), a.end(),[](int x){return x>0;})<<"\n";
+	// returns 1 if any one of the element of the vector
+	// satisfies the given function.
+
+	cout<< none_of(a.begin(), a.end(),[](int x){return x>0;})<<"\n";
+	// returns 1 if none of the elements of the vector satisfies
+	// the given function.
+
+	// These functions also work for arrays, we only need to
+	// change 'a.begin()' with 'a' and 'a.end()' with 'a+n'.
+	
+	return 0;
+} 
---
+++ allabout_strings.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // INPUT AND OUTPUT
+    // string s;
+    // getline(cin, s);
+    // cout << s;
+
+    // CONCATENATION (USING APPEND)
+    // string s1 = "fam";
+    // string s2 = "ily";
+    // s1.append(s2);
+    // cout << s1 << endl;
+
+    // CONCATENATION (BETTER METHOD)
+    // string s1 = "fam";
+    // string s2 = "ily";
+    // cout << s1 + s2 << endl;
+    // s1 = s1 + s2;
+    // cout << s1 << endl;
+    // cout << s2 << endl;
+    // cout << s1[2] << endl;
+
+    // COMPARING STRINGS
+    // string str1 = "abc";
+    // string str2 = "xyz";
+    // if (str1.compare(str2) == 0)
+    //     cout << "Strings are equal" << endl;
+    // else
+    //     cout << "Not equal" << endl;
+
+    // if (!str1.compare(str2)) // Whenever we equate something with zero(0), such a code can be used as zero means false.
+    //     cout << "Strings are equal" << endl;
+    // else
+    //     cout << "Not equal" << endl;
+
+    // Empty string
+    // string s = "hello user";
+    // cout << s << endl;
+
+    // s.clear();
+    // if ( s.empty()) //It will return 1 if string is empty and 0 if it isn't
+    //     cout << "String is empty" << endl;
+
+    // Erase function
+    //     string s = "nincompoop"; // foolish person
+    //     s.erase(3, 4);           // focus on the arguments of the function
+    //     cout << s << endl;
+
+    // find function
+    // string s = "nincompoop";
+    // cout << s.find("poop") << endl;
+
+    // insert function
+    // string s = "Hello user";
+    // s.insert(3, "lol");
+    // cout << s << endl;
+    // s.erase(3, 3);
+    // s.insert(5, "lol");
+    // cout << s << endl;
+    // s.erase(5, 3);
+    // s.insert(6, "lol");
+    // cout << s << endl;
+
+    // SIZE OF A STRING
+    // The size() function is consistent with other STL containers
+    // (like vector, map, etc.) and length() is consistent with most
+    // peoples intuitive notion of character strings like a word, sentence
+    // or paragraph. We say a paragraph's length not its size, so length()
+    // is to make things more readable.
+    // string s = "Hello user";
+    // cout << s.size()<< endl;
+    // for (int i = 0; i < s.length(); i++)
+    //     cout << s[i] << endl;
+
+    // SUBSTRING
+    // string s = "nincompoop";
+    // string s1 = s.substr(6, 4);
+    // cout << s1 << endl;
+
+    // STRING TO INTEGER AND INTEGER TO STRING
+    string s = "780";
+    int n = stoi(s);
+    cout << n + 2 << endl;
+    cout << to_string(n) + "2" << endl;
+
+    // Sorting a string using #include<algorithm> header file
+    // string s1 = "acfnsubvubnuhdg";
+    // sort(s1.begin(), s1.end());
+    // cout << s1 << endl;
+}
