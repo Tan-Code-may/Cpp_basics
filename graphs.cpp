7b135921612cac487e2ed2f0f3967a655d8065b6
Tanmay Garg <95227013+Tan-Code-may@users.noreply.github.com>
2022-07-10 00:02:01 +0530

    graph algo uploaded

---
+++ graphs.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//IMPORTANT POINTS
+// 1. A directed graph may be acyclic even after having a circular part. 
+// 2. Acyclic undirected graphs are known as trees.
+// 3. A stongly connected component is the one in which we can traverse any node from any other node.
+
+
+// Adjacency Matrix
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	int graph[n+1][n+1];
+	memset(graph,0,sizeof(graph));
+	for(int i=0;i<m;i++)
+	{
+		int v1,v2;
+		cin>>v1>>v2;
+		graph[v1][v2] = 1;
+		graph[v2][v1] = 1;
+	} 
+	for(int i=0;i<n+1;i++)
+	{
+		for(int j=0;j<n+1;j++)
+			cout<<graph[i][j]<<" ";
+		cout<<"\n";
+	}
+
+	return 0;
+}
