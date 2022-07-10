bb0abc15265ca661d21e25478e69ec44d7d33477
Tanmay Garg <95227013+Tan-Code-may@users.noreply.github.com>
2022-07-10 18:25:28 +0530

    dfs upload

--- graphs.cpp
+++ graphs.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N=1e3 +10;
+vector<int> g[N];
+bool vis[N];
 
 //IMPORTANT POINTS
 // 1. A directed graph may be acyclic even after having a circular part. 
@@ -35,27 +38,57 @@ using namespace std;
 
 
 // Adjacency list
+// int main()
+// {
+//     int n, m;
+//     cin >> n >> m;
+//     vector<int> v[n + 1];
+
+//     for (int i = 0; i < m; i++)
+//     {
+//         int v1, v2;
+//         cin >> v1 >> v2;
+//         v[v1].push_back(v2);
+//         v[v2].push_back(v1);
+//     }
+
+//     for (int i = 0; i < n + 1; i++)
+//     {
+//         cout << i << ": ";
+//         for (int j = 0; j < v[i].size(); j++)
+//             cout << v[i][j] << " ";
+//         cout << "\n";
+//     }
+
+//     return 0;
+// }
+
+void dfs(int vertex)
+{
+	vis[vertex]= true;
+	cout<<vertex<<" ";
+	for(int child: g[vertex])
+	{
+		if(vis[child])
+			continue;
+		dfs(child);
+	}
+	// Time complexity- O(n+e)
+}
+
+// DFS traversal
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<int> v[n + 1];
+	int n,m;
+	cin>>n>>m;
 
-    for (int i = 0; i < m; i++)
-    {
-        int v1, v2;
-        cin >> v1 >> v2;
-        v[v1].push_back(v2);
-        v[v2].push_back(v1);
-    }
-
-    for (int i = 0; i < n + 1; i++)
-    {
-        cout << i << ": ";
-        for (int j = 0; j < v[i].size(); j++)
-            cout << v[i][j] << " ";
-        cout << "\n";
-    }
-
-    return 0;
+	for(int i=0;i<m;i++)
+	{
+		int v1,v2;
+		cin>>v1>>v2;
+		g[v1].push_back(v2);
+		g[v2].push_back(v1);
+	} 
+	dfs(1);
+	return 0;
 }
