d54f65df14f22eaaa5f89fce7a3a724f8840d9f0
Tanmay Garg <95227013+Tan-Code-may@users.noreply.github.com>
2022-07-10 16:18:54 +0530

    adjacency list added

--- graphs.cpp
+++ graphs.cpp
@@ -8,25 +8,54 @@ using namespace std;
 
 
 // Adjacency Matrix
+// int main()
+// {
+// 	int n,m;
+// 	cin>>n>>m;
+// 	int graph[n+1][n+1];
+// 	memset(graph,0,sizeof(graph));
+//
+// 	for(int i=0;i<m;i++)
+// 	{
+// 		int v1,v2;
+// 		cin>>v1>>v2;
+// 		graph[v1][v2] = 1;
+// 		graph[v2][v1] = 1;
+// 	} 
+//
+// 	for(int i=0;i<n+1;i++)
+// 	{
+// 		for(int j=0;j<n+1;j++)
+// 			cout<<graph[i][j]<<" ";
+// 		cout<<"\n";
+// 	}
+
+// 	return 0;
+// }
+
+
+// Adjacency list
 int main()
 {
-	int n,m;
-	cin>>n>>m;
-	int graph[n+1][n+1];
-	memset(graph,0,sizeof(graph));
-	for(int i=0;i<m;i++)
-	{
-		int v1,v2;
-		cin>>v1>>v2;
-		graph[v1][v2] = 1;
-		graph[v2][v1] = 1;
-	} 
-	for(int i=0;i<n+1;i++)
-	{
-		for(int j=0;j<n+1;j++)
-			cout<<graph[i][j]<<" ";
-		cout<<"\n";
-	}
+    int n, m;
+    cin >> n >> m;
+    vector<int> v[n + 1];
 
-	return 0;
+    for (int i = 0; i < m; i++)
+    {
+        int v1, v2;
+        cin >> v1 >> v2;
+        v[v1].push_back(v2);
+        v[v2].push_back(v1);
+    }
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        cout << i << ": ";
+        for (int j = 0; j < v[i].size(); j++)
+            cout << v[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
 }
