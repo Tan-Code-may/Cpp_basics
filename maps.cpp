b74149974494fb8c17bbc1161f3c3a5cf0bdcf5d
Tanmay Garg <95227013+Tan-Code-may@users.noreply.github.com>
2022-07-26 14:27:45 +0530

    unordered map updated

--- maps.cpp
+++ maps.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     // In maps, unique key-value pairs are stored.
-    // They are always stored in sorted form with respect to key.(For ex. Strings are stored in lexicographical order if they are passed as key)
+    // They are always stored in sorted form with respect to key.(For ex. Strings are stored in lexicographical order if they are passed as key).
 	// Maps are not necessarily alloted continous memory locations.
 	// Internally implementation of maps takes place using 'Red-black trees', and that is why the TC of all the operations of maps is O(log(n)).
 
@@ -49,6 +49,27 @@ int main()
     // is inserted in sorted manner. Now, this will also consume some time, depending upon the length of the key string.
     map<string,string> ms;
     ms["gnofejas"] = "bgdafjks";
+	
+	
+	
+	// UNORDERED MAP
+    // 1. Inbuilt implementation- Using HASH TABLES
+    // 2. Time complexity- reduces to O(1) in operations of unordered_map, which are same as that of map (average).
+    // 3. Valid key datatype- Complex data types, such as, pairs cannot be used as key in unordered_map, unlike map as no HASH function for pair is available.
+
+    unordered_map<int,string> m;
+
+    //TC- O(1);
+    // Everything is same in both map and unordered_map, except the fact that in map key-value pairs are stored in sorted manner.
+    // Therefore, until pairs are not required in sorted manner, unordered_map should always be preferred.
+    m[3]="abd";
+    m[1]="sfd";
+    m[7]="ehts";
+    m[5]="ger";
+    
+    // Key-value pairs are printed randomly in unordered map (not necessarily in the order that we have inserted them inside the map)
+    for(auto &pr: m)
+    	cout<<pr.first<<" "<<pr.second<<"\n";
 
     return 0;
 }
